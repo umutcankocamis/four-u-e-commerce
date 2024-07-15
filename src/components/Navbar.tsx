b909cb19774697c8faf42bcc8f7aef8bdfbf54ec
Umutcan Kocamış <umutceaan@gmail.com>
2024-07-15 14:29:49 +0300

    changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
           </Link>
           <div className="hidden xl:flex gap-4">
             <Link href="/">Home</Link>
-            <Link href="/">Shop</Link>
+            <Link href="/list">Shop</Link>
             <Link href="/">Deals</Link>
             <Link href="/">About</Link>
             <Link href="/">Contact</Link>
